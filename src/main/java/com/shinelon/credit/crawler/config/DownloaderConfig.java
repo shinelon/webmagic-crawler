4c909d4978ed9d0c47dbc13c60b9318cf9caf7a1
shinelon <syq_sunshine@163.com>
2018-11-29 15:14:43 +0800

    add chrome driver version

--- src/main/java/com/shinelon/credit/crawler/config/DownloaderConfig.java
+++ src/main/java/com/shinelon/credit/crawler/config/DownloaderConfig.java
@@ -1,7 +1,11 @@
 package com.shinelon.credit.crawler.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.shinelon.credit.crawler.chrome.ChromeDriverDownloader;
 
 import us.codecraft.webmagic.downloader.HttpClientDownloader;
 
@@ -15,19 +19,24 @@ import us.codecraft.webmagic.downloader.HttpClientDownloader;
 @Configuration
 public class DownloaderConfig {
 
-    @Bean(name = "spiderHttpClientDownloader")
-    public HttpClientDownloader config() {
-        HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
-        httpClientDownloader.setThread(8);
-        // httpClientDownloader.setProxyProvider(proxyProvider);
-        return httpClientDownloader;
-    }
-    
-    @Bean(name = "spiderSeleniumDownloader")
-    public SeleniumDownloader configSelenium() {
-    	SeleniumDownloader downloader=new SeleniumDownloader("D:\\eclipse-workspace2018-photon\\chromedriver_win32\\chromedriver.exe");
-    	downloader.setThread(1);
-    	downloader.setSleepTime(1000);
-    	return downloader;
-    }
+	@Value("${chrome.driver.path}")
+	private String chromeDriverPath;
+
+	@Bean(name = "spiderHttpClientDownloader")
+	public HttpClientDownloader config() {
+		HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
+		httpClientDownloader.setThread(8);
+		// httpClientDownloader.setProxyProvider(proxyProvider);
+		return httpClientDownloader;
+	}
+
+	@Bean(name = "spiderChromeDriverDownloader")
+	public ChromeDriverDownloader configSelenium() {
+		if (StringUtils.isBlank(chromeDriverPath)) {
+			throw new RuntimeException("chrome.driver.path is blank ,please check application.properties ！");
+		}
+		ChromeDriverDownloader downloader = new ChromeDriverDownloader(chromeDriverPath);
+		downloader.setThread(8);
+		return downloader;
+	}
 }
